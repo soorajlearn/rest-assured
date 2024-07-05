84de0f9c6bf89273a05acb425738ff4bdc184a31
soorajlearn <soorajlearn@gmail.com>
2024-07-05 20:54:52 +0530

    Update ContentType.java

--- rest-assured/src/main/java/io/restassured/http/ContentType.java
+++ rest-assured/src/main/java/io/restassured/http/ContentType.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * testing github changes
+ * second commit
  */
 
 package io.restassured.http;
